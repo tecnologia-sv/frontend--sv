f8f89cc4033e6c6668e2498ec333349d9d849a7e
diegoherrera262 <diegoherrera262@gmail.com>
2021-07-13 16:45:34 -0500

    feature: renamed components directory

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
 
- import react  from  'react'
+import react from 'react'
 
 import './App.css';
 import CreateZoneLeaderForm from './components/CreateZoneLeaderForm';
 
 // Default initial values for the form
 const defaultInitialValues = {
-    name : '',
-    lastName : '',
-    documentId : '',
-    zone : '',
-    address : '',
-    leaderCode : '',
-    email : '',
-    cellphone : '',
-    endContractDate : '',
+  name: '',
+  lastName: '',
+  documentId: '',
+  zone: '',
+  address: '',
+  leaderCode: '',
+  email: '',
+  cellphone: '',
+  endContractDate: '',
 }
 
 // Keys for labeling inputs
 const labelKeys = {
-    name : 'Nombre',
-    lastName : 'Apellido',
-    documentId : 'Documento de identidad',
-    zone : 'Zona',
-    address : 'Dirección',
-    leaderCode : 'Código de líder',
-    email : 'Email',
-    cellphone : 'Número celular',
-    endContractDate : 'Fecha fin de contrato',
+  name: 'Nombre',
+  lastName: 'Apellido',
+  documentId: 'Documento de identidad',
+  zone: 'Zona',
+  address: 'Dirección',
+  leaderCode: 'Código de líder',
+  email: 'Email',
+  cellphone: 'Número celular',
+  endContractDate: 'Fecha fin de contrato',
 }
 
 const typeKeys = {
-    name : 'text',
-    lastName : 'text',
-    documentId : 'number',
-    zone : 'select',
-    address : 'text',
-    leaderCode : 'number',
-    email : 'email',
-    cellphone : 'number',
-    endContractDate : 'date',
+  name: 'text',
+  lastName: 'text',
+  documentId: 'number',
+  zone: 'select',
+  address: 'text',
+  leaderCode: 'number',
+  email: 'email',
+  cellphone: 'number',
+  endContractDate: 'date',
 }
 
 const selectValues = {
-    zone : [
-      '--Elija una zona--',
-      'Norte',
-      'Sur'
-    ]
+  zone: [
+    '--Elija una zona--',
+    'Norte',
+    'Sur'
+  ]
 }
 
 /*
